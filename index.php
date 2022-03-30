af7591ce7cd4a8be7a4daac2ec5e4f9f0ab3bcd3
Mikhail Samorodov <masamorodov@gmail.com>
2022-03-30 12:49:50 +0300

    Вносит правки

--- index.php
+++ index.php
@@ -5,10 +5,8 @@ $title = 'Главная страница';
 require_once('data.php');
 require_once('helpers.php');
 
-$content = include_template('main.php', ['products' => $products, 'categories'=> $categories] );
+$content = include_template('main.php', ['lots' => $lots, 'categories'=> $categories] );
 $layout_content = include_template('layout.php',['content' => $content, 'user_name' => $user_name, 
 'categories'=> $categories, 'title'=>$title]);
 
 print($layout_content);
- 
-?>
